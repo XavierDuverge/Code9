bbb03656aef5a0d03187ce3525eb46c0777ed2a7
XavierDuverge <xavierduvergemontero2@gmail.com>
2025-10-25 23:23:25 -0400

    Sube versión actualizada del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import EstadisticasEST from "./Pages/EstadisticasEST";
+function App() {
+  return (
+    <div className="bg-white h-screen flex items-center justify-center">
+      <EstadisticasEST/>
+    </div>
+  )
+}
+
+export default App;
